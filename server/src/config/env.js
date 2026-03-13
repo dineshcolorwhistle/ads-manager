2fb99ffe3ab78241ba90d1a0cb1d66c0b5a9e772
dinesh <dinesh.colorwhistle@gmail.com>
2026-03-13 14:39:31 +0530

    server config issue fixed

--- server/src/config/env.js
+++ server/src/config/env.js
@@ -7,7 +7,9 @@ const path = require('path');
  */
 
 // Load .env file
-require('dotenv').config();
+require('dotenv').config({
+    path: require('path').resolve(__dirname, '../../.env')
+});
 
 /**
  * Get environment variable with validation
